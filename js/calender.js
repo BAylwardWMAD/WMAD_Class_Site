6e7985abfbbd1093744d496adbe37006d5e0c907
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-08-21 19:04:35 -0300

    commit

--- js/calender.js
+++ js/calender.js
@@ -1,27 +1,73 @@
 const date = new Date();
 
-const monthDays = document.querySelector('.days');
-const months = [
-    'January',
-    'Feburary',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-];
+const loadCalendar = () => {
+    date.setDate(1);
 
-document.querySelector('.date h1').innerHTML = months[date.getMonth()];
-document.querySelector('.date p').innerHTML = date.toDateString();
+    const monthDays = document.querySelector('.days');
 
-let days = "";
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
-for (let i = 1; i <= 31; i++) {
-    days += `<div>${i}</div>`;
+    const prevMonthLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+    const firstDayNum = date.getDay();
+
+    const lastDayNum = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+
+    const nextDays = 7 - lastDayNum - 1;
+
+    const months = [
+        'January',
+        'Feburary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+
+    document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+    document.querySelector('.date p').innerHTML = new Date().toDateString();
+
+    let days = "";
+
+    for (let x = firstDayNum; x > 0; x--) {
+        days += `<div class="prev-date">${prevMonthLastDay - x + 1}</div>`
+    }
+    for (let i = 1; i <= lastDay; i++) {
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today">${i}</div>`;
+        } else {
+            days += `<div>${i}</div>`;
+        }
+    }
+
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`
+    }
     monthDays.innerHTML = days;
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    loadCalendar();
+});
+document.querySelector('.next').addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    loadCalendar();
+});
+
+loadCalendar();
+
+const daysArray = document.querySelector('.days').children;
+
+for (let i = 0; i < daysArray.length; i++) {
+    daysArray[i].addEventListener('click', () => {
+        daysArray[i].classList.toggle('today');
+    })
+
 }
