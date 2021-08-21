d85054d9f8b935d528b0e823ec379fb192af19ec
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-08-21 16:38:05 -0300

    commit

--- js/calender.js
+++ js/calender.js
@@ -1,80 +1,27 @@
-document.querySelector(function() {
-    document.querySelector('.calendar-container').calendar();
-});
-document.querySelector('.calendar-container').calendar({
-    date: new Date()
-});
-document.querySelector('.calendar-container').calendar({
-    weekDayLength: 2
-});
+const date = new Date();
 
-document.querySelector('.calendar-container').calendar({
-    date: new Date(),
-    disable: function(date) {
-        return date < new Date();
-    }
-});
+const monthDays = document.querySelector('.days');
+const months = [
+    'January',
+    'Feburary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
 
-document.querySelector('.calendar-container').calendar({
-    onClickDate: function(date) {
-        // do something    
-    }
-});
+document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+document.querySelector('.date p').innerHTML = date.toDateString();
 
-document.querySelector('.calendar-container').calendar({
+let days = "";
 
-    // text for prev/next buttons
-    prevButton: "Prev",
-    nextButton: "Next",
-
-    // custom separator between the month and the year in the month view.
-    monthYearSeparator: " ",
-
-    // false = 4 months in a row
-    showThreeMonthsInARow: true,
-
-    // whether to change either month or year
-    enableMonthChange: true,
-
-    // whether to disable year view
-    enableYearView: true,
-
-    // shows a Today button on the bottom of the calendar
-    showTodayButton: true,
-    todayButtonContent: "Today",
-
-    // highlights all other dates with the same week-day
-    highlightSelectedWeekday: true,
-
-    // highlights the selected week that contains the selected date
-    highlightSelectedWeek: true,
-
-    // whether to enable/disable the year selector
-    showYearDropdown: false,
-
-    // min/max dates
-    // Date Object or Date String
-    min: null,
-    max: null,
-
-    // start on Sunday instead
-    startOnMonday: false,
-
-});
-
-document.querySelector('.calendar-container').calendar({
-
-    onChangeMonth: function(date) {},
-    onClickToday: function(date) {},
-    onClickMonthNext: function(date) {},
-    onClickMonthPrev: function(date) {},
-    onClickYearNext: function(date) {},
-    onClickYearPrev: function(date) {},
-    onShowYearView: function(date) {},
-    onSelectYear: function(date) {},
-
-});
-
-var calendar = document.querySelector('.calendar-container').calendar();
-
-console.log(calendar.getSelectedDate());
+for (let i = 1; i <= 31; i++) {
+    days += `<div>${i}</div>`;
+    monthDays.innerHTML = days;
+}
