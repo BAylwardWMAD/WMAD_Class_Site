35bad3491e3d37e6a12c298dd62f2030e06fedec
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-08-20 21:22:29 -0300

    commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.navList');
+const navLines = document.querySelectorAll('.hamburger_line');
+
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle('toggle');
+    navLines[0].classList.toggle('line1');
+    navLines[1].classList.toggle('line2');
+})
