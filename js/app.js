b738421b7c651689918dd3223e09a6fbe2357b4d
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-08-22 11:32:33 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ import * as calender from './calender.js';
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.navList');
 const navLines = document.querySelectorAll('.hamburger_line');
+const sideBarBtn = document.querySelector('.id-side-btn');
+const sideBar = document.querySelector('.important-dates_side-bar');
 
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('toggle');
     navLines[0].classList.toggle('line1');
     navLines[1].classList.toggle('line2');
+});
+
+sideBarBtn.addEventListener('click', () => {
+    console.log('test');
+    sideBar.classList.toggle('slide-out');
 });
 
 if (document.body.classList.contains('schedule_body')) {
