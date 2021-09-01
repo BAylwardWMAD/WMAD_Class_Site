4213a15ce8ee4f2363d2e8dc15989c11a5b12808
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-08-31 23:32:58 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -14,6 +14,20 @@ hamburger.addEventListener('click', () => {
     navLines[1].classList.toggle('line2');
 });
 
+document.body.addEventListener('click', (event) => {
+    let clickedInNavList = navList.contains(event.target);
+    let clickedInNavBar = navBar.contains(event.target);
+
+    if (!clickedInNavList && !clickedInNavBar) {
+        if (navList.classList.contains('toggle')) {
+            navList.classList.remove('toggle');
+            navLines[0].classList.remove('line1');
+            navLines[1].classList.remove('line2');
+        };
+    };
+
+});
+
 let prevScrollpos = window.pageYOffset;
 window.addEventListener('scroll', () => {
     let currentScrollPos = window.pageYOffset;
